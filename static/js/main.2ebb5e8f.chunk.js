5a2fd3ea9cb9444f58a52dc585b5877934342d86
repro-tetris <77537560+repro-tetris@users.noreply.github.com>
2021-09-03 23:33:13 +0300

    Updates

--- static/js/main.2ebb5e8f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(0));var a=function(){return Object(s.jsx)("div",{})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.2ebb5e8f.chunk.js.map
