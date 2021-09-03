72352505f469526cb89c0f3089b03fac6ed3cfcd
repro-tetris <77537560+repro-tetris@users.noreply.github.com>
2021-09-03 23:32:14 +0300

    fix styles

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -4,4 +4,10 @@ export const Label = styled.label`
   display: flex;
   flex-direction: column;
   margin-bottom: 10px;
+  + input[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      background-color: aquamarine;
+    }
+  }
 `;
